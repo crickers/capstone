b1c995912f61a663433e650fec46c794432bc77b
Cricky <crickmonster@gmail.com>
2021-05-06 18:54:53 -0400

    Login Button Built, clicks to nowhere

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import LoginButton from ".components/LoginButton";
+import LoginButton from "./components/LoginButton";
 
 function App() {
   return <LoginButton />;
