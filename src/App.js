a0fcf6ff5725ef39bbcc7fd07c343887ab5cd015
Cricky <crickmonster@gmail.com>
2021-05-06 20:35:28 -0400

    added import { useAuth0 } to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
   return (
-    <LoginButton />
-    <LogoutButton />
+    <>
+    < LoginButton />
+    < LogoutButton />
+    </>
   );
 }
 
