a413dc724d6cedfe3f561663ea8823e55663b23a
Cricky <crickmonster@gmail.com>
2021-05-11 12:22:01 -0400

    attempt to fix errors

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = process.env.REACT_APP_AUTH0_DOMAIN=dev-thecrick.us.auth0.com;
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID= 1Ua3irUsXMhC6uKEfDTyaF6GQNEjCLha;
 
 ReactDOM.render(
   <Auth0Provider
-    domain={domain}
-    clientID={clientID}
+    domain="dev-thecrick"
+    clientID={1Ua3irUsXMhC6uKEfDTyaF6GQNEjCLha}
     redirectURI={window.location.origin}
   >
     <App />
